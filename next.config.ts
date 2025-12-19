54005c6c8aada2426fbb23a066dbfe6d844a60f1
Sokkhann <sokkhann.pol02@gmail.com>
2025-12-19 13:50:07 +0700

    update verson next

--- next.config.ts
+++ next.config.ts
@@ -1,12 +1,7 @@
+// next.config.ts
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  "compilerOptions": {
-    "baseUrl": ".",
-    "paths": {
-      "@/*": ["./src/*"]
-    }
-  }
 };
 
 export default nextConfig;
