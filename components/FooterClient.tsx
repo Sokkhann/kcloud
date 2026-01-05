2ee617090f4526880c501741cd5102d977631d1f
Sokkhann <sokkhann.pol02@gmail.com>
2026-01-05 08:44:31 +0700

    update merge code for pre production demo

--- components/FooterClient.tsx
+++ components/FooterClient.tsx
@@ -3,8 +3,7 @@ import React, { useState } from "react";
 import { Facebook, Linkedin, Send, ChevronDown } from "lucide-react";
 import Image from "next/image";
 import Link from "next/link";
-import { NavbarProducts } from "@/type/dataTypes";
-
+import { additionalMenuItems } from "@/data/additional-data/menuItems"
 // ----------------------------------------------------------------------
 // 1. Reusable Component for List Sections (Now defaults to Mobile Dropdown)
 // ----------------------------------------------------------------------
@@ -69,66 +68,6 @@ const FooterColumn = ({
     </div>
   );
 };
-
-
-const additionalMenuItems: NavbarProducts[] = [
-  {
-    title: "Virtual Machine",
-    category: "Compute",
-    path: "/products/virtual-machine",
-    image: "/feature-carousel-vm.png",
-    description: "Deploy scalable, high-performance virtual computing instances in seconds."
-  },
-  {
-    title: "Kubernetes",
-    category: "Compute",
-    path: "/products/kubernetes",
-    image: "/feature-carousel-k8.png",
-    description: "Fully managed container orchestration to simplify deploying and scaling applications."
-  },
-  {
-    title: "IP Address",
-    category: "Networking",
-    path: "/products/ip-address",
-    image: "/feature-carousel-ip.png",
-    description: "Manage reserved public and private IP addresses for your cloud infrastructure."
-  },
-  {
-    title: "Load Balancer",
-    category: "Networking",
-    path: "/products/load-balancer",
-    image: "/feature-carousel-load-balancer.png",
-    description: "Optimize application availability and performance by intelligently distributing incoming traffic."
-  },
-  {
-    title: "VPC",
-    category: "Networking",
-    path: "/products/vpc",
-    image: "/feature-carousel-vm.png",
-    description: "Isolate your cloud resources within a secure, private network environment."
-  },
-  {
-    title: "Block Storage",
-    category: "Storage",
-    path: "/products/block-storage",
-    image: "/feature-carousel-block-storage.png",
-    description: "High-performance, scalable SSD storage for your high-demand applications."
-  },
-  {
-    title: "Block Storage Snapshot",
-    category: "Storage",
-    path: "/products/snapshot",
-    image: "/feature-carousel-block-storage-snapshot.png",
-    description: "Capture point-in-time copies of your block storage volumes for easy data recovery."
-  },
-  {
-    title: "Virtual Machine Backup",
-    category: "Storage",
-    path: "/products/vm-backup",
-    image: "/feature-carousel-vm.png",
-    description: "Full-system backups for your virtual machines to prevent data loss."
-  },
-];
 
 // ----------------------------------------------------------------------
 // 2. Main Footer Component
